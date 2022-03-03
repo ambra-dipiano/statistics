4c1982ec855a98ea87eaabe05dee69d8a6f3d18b
ambra-dipiano <ambra.dipiano@inaf.it>
2022-03-03 09:36:40 +0100

    initial commit

---
+++ onetailed-pvalues.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.stats import norm
+
+def get_sigma_from_pvalue(pval, decimals=3):
+    return np.abs(np.round(norm.ppf(pval), decimals))
+
+def get_prob_from_sigma(sigma, decimals=8):
+    return np.round(1-(norm.sf(sigma)*2), decimals)
+
+def get_prob_from_pvalue(pval, decimals=8):
+    return np.round(1-pval*2, decimals)
+
+def get_pvalue_from_sigma(sigma, decimals=8):
+    p = get_prob_from_sigma(sigma, decimals=decimals)
+    return np.round((1-p)/2, decimals)
+
+
+sigma = 3
+prob = 0.9973
+pval = np.round((1-prob)/2, 8)
+
+print(f'1-Tail probability of {sigma} sigma = {get_prob_from_sigma(sigma)}')
+print(f'1-Tail probability of {pval} pvalue = {get_prob_from_pvalue(pval)}')
+print(f'{pval} pvalue = {get_sigma_from_pvalue(pval)} sigma')
+print(f'{sigma} sigma = {get_pvalue_from_sigma(sigma)} pvalue')
